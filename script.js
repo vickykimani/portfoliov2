2b09f1b507b9d7b0d2d085c28b10694c0f6596ca
vickykimani <kyle@pearsonrealtyholdings.com>
2025-07-04 19:58:53 -0700

    smooth scrolling js for nav links

---
+++ script.js
@@ -0,0 +1,11 @@
+//smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behaviour: 'smooth',
+            block: 'start'
+        });
+    });
+});
